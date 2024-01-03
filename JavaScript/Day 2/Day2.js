73cf81d86a26494e5cad2ef74b7fa675531e7213
KasinathCR <crkasinath@live.com>
2024-01-03 13:37:37 +0530

    Day 2

---
+++ JavaScript/Day 2/Day2.js
@@ -0,0 +1,84 @@
+//JavaScript Precedence Rules
+var result = 0;
+
+result = 1 + (3 * 4);
+console.log(result);
+
+result = (1 + 3) * 4;
+console.log(result);
+
+result = (2 * 4) * 4 + 2;
+console.log(result);
+
+result = ((2 * 4) * 4) + 2;
+console.log(result);
+
+result = (2 * 4) * (4 + 2);
+console.log(result);
+
+//Concatenating Text Strings
+var name = "Kasinath"
+var greeting = "Hello "
+var output = greeting + name;
+console.log(output);
+
+output = "Welcome "+ name;
+console.log(output);
+
+output = "2 plus 2 is: "+ 2 + 2;
+console.log(output);
+
+//Prompts
+var getName = prompt("What is your name?", "Kasinath Conjeevaram Ravi");
+console.log(getName);
+
+var getNumberAsString = prompt("What is 1 + 1?")
+console.log(getNumberAsString);
+
+var getNumberAsString = prompt("How many dogs do you have?", 1);
+var getNumberAsConcatenatedString = getNumberAsString + 1;
+console.log(getNumberAsConcatenatedString);
+
+var getNull = window.prompt("???"); //user presses cancel
+console.log(getNull);
+
+//if Statements
+var getAnswer = prompt("What is the Capital of TamilNadu?");
+if (getAnswer === "Chennai")
+{
+    console.log("That's Correct!");
+}
+
+//Comparison Operators
+var name1 = "Kasinath";
+var name2 = "Kasinath";
+if (name1 === name2)
+{
+    console.log("The names are same");
+}
+name2 = "kasinath"
+if (name1 !== name2)
+{
+    console.log("The names are different");
+}
+var num1 = 0;
+var num2 = 1;
+if (num1 < num2)
+{
+    console.log("Num1 is less than Num2");
+}
+num1 = 1;
+if (num1 <= num2)
+{
+    console.log("Num1 is less than or equal to Num2");
+}
+num2 = 0
+if (num1 > num2)
+{
+    console.log("Num1 is greater than Num2");
+}
+num2 = 1;
+if (num1 >= num2)
+{
+    console.log("Num1 is greater than or equal to Num2");
+}
